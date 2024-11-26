1fb86f1f137d316102dcdd8e3e08a7bba3118918
chrisbrierley <c.brierley@ucl.ac.uk>
2024-11-26 16:40:38 +0000

    Getting stuff ready to teach in NWW110A again

---
+++ onetime_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s /data/oacd/geog0121 data
+
+conda create -n geog0121 -y
+conda activate geog0121
+conda install -y -c conda-forge cartopy cftime jupyter nb_conda nc-time-axis matplotlib regionmask urllib3 xarray xclim
