b54b353cd56daece041e5cba4ec5cef4e7bf819b
Chris Brierley <c.brierley@ucl.ac.uk>
2024-12-11 14:20:51 +0000

    Pointing to /opt/conda_envs

--- onetime_setup.sh
+++ onetime_setup.sh
@@ -2,6 +2,8 @@
 
 ln -s /data/oacd/geog0121 data
 
-conda create -n geog0121 -y
-conda activate geog0121
-conda install -y -c conda-forge cartopy cftime jupyter nb_conda nc-time-axis matplotlib regionmask urllib3 xarray xclim
+#conda create -n geog0121 -y
+#conda activate geog0121
+#conda install -y -c conda-forge cartopy cftime jupyter nb_conda nc-time-axis matplotlib regionmask urllib3 xarray xclim
+
+conda config --add envs_dirs /opt/conda_envs
