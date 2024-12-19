a5e5fac557e9078994cd4037e0322483b421a5a6
chrisbrierley <c.brierley@ucl.ac.uk>
2024-12-19 12:23:51 +0000

    Still working on getting the class set up

--- onetime_setup.sh
+++ onetime_setup.sh
@@ -2,8 +2,13 @@
 
 ln -s /data/oacd/geog0121 data
 
-#conda create -n geog0121 -y
-#conda activate geog0121
-#conda install -y -c conda-forge cartopy cftime jupyter nb_conda nc-time-axis matplotlib regionmask urllib3 xarray xclim
+# conda create -n geog0121 -y
+# conda activate geog0121
+# conda install -y -c conda-forge cartopy cftime jupyter nb_conda nc-time-axis matplotlib regionmask urllib3 xarray xclim
 
-conda config --add envs_dirs /opt/conda_envs
+conda config --add envs_dirs /opt/conda_envs/
+
+cd Desktop
+ln -s /data/oacd/geog0121 geog0121_data
+ln -s /opt/PanoplyJ/panoply.sh panoply
+ln -s ~/Documents/geog0121/open_geog0121_JupyterLab.sh geog0121_JupyterLab 
---
+++ open_geog0121_JupyterLab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+
+cd ~/Documents/geog0121
+conda activate geog0121
+jupyter lab ~/Documents/geog0121
